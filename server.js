14b9121f664d85b279ea6a3a1abac54f35781f80
Pablo Muñoz Villegas <75706875+Pablonio@users.noreply.github.com>
2024-02-21 14:05:16 -0400

    cambios

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'); // Importa el framework Express para crear e
 const axios = require('axios'); // Importa la biblioteca Axios para realizar solicitudes HTTP.
 const cors = require('cors'); // Importa el middleware CORS para permitir solicitudes desde otros dominios.
 const app = express(); // Crea una instancia de la aplicación Express.
-const port = 3001; // Define el puerto en el que el servidor escuchará las solicitudes.
+const port = "https://server-beta-hazel.vercel.app/"; // Define el puerto en el que el servidor escuchará las solicitudes.
 
 app.use(express.json()); // Agrega middleware para analizar el cuerpo de las solicitudes como JSON.
 app.use(cors()); // Activa el middleware CORS para permitir solicitudes desde cualquier origen.
@@ -35,5 +35,4 @@ app.post('/preguntar', async (req, res) => { // Define una ruta para manejar las
 });
 
 app.listen(port, () => {
-    
 });
